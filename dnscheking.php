47d33a6530b5fd5d2aab207716f855db7155628e
alisedighi <ali.sedighi@gmail.com>
2014-08-31 22:35:24 -0700

    Initial DNS checking script

---
+++ dnscheking.php
@@ -0,0 +1,10 @@
+#Author:      Ali Sedi        
+#Language:    PHP
+#Date:        31 August 2013  
+#--------------------------------------------------------------------
+#Description: Script reads the named.conf file and and check domains
+#             and assosiated dns. 
+#Purpose:     Check if all the zone files are using a certain dns
+#Input:       A string which is named.conf file   
+#Output:      A string that contains a list of domains
+#####################################################################
