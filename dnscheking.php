06906c130729c24a74d2ee2e0e2325c7db82696f
ali sedi <ali.sedighi@gmail.com>
2014-09-01 22:27:09 -0700

    Initial the script
    Script open named.conf to read it and looks for lines which start with
    zone string.

--- dnscheking.php
+++ dnscheking.php
@@ -1,3 +1,5 @@
+<?php
+/*
 #Author:      Ali Sedi        
 #Language:    PHP
 #Date:        31 August 2013  
@@ -5,6 +7,27 @@
 #Description: Script reads the named.conf file and and check domains
 #             and assosiated dns. 
 #Purpose:     Check if all the zone files are using a certain dns
-#Input:       A string which is named.conf file   
+#Input:       A file which is named.conf file   
 #Output:      A string that contains a list of domains
 #####################################################################
+*/
+echo "reading the file<br>";
+$myfile = fopen("named.conf","r")or die ("unable to open the file!");
+{	 
+	while(!feof($myfile))	
+	{	
+		$line =  stream_get_line($myfile , 4096,"\n");
+		echo $line;
+		if(preg_match('/^zone/i',$line))
+		{
+			echo "<u> zone</u><br>";
+		}
+		else
+		{
+			echo "<b>no zone</b><br>";
+		}
+	}
+	fclose ($myfile);	
+}
+
+?>
