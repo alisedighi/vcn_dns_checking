e5c76552f58d763c4bf7090ba63572e55e594fa8
ali sedi <ali.sedighi@gmail.com>
2014-09-05 22:31:32 -0700

    Add a whois function
    The whois function is able to retrieve correct information about the
    given IP. This function communicates with whois.iana.org and ask for the
    RIR whois server of the IP address.Then the whois server of the RIR and
    get the whois details of the IP address.

--- dnscheking.php
+++ dnscheking.php
@@ -33,8 +33,59 @@ function aorptr ($str)
 	//	echo gethostbyname($str)."<br>";
 	    }
 }
+
+function getwhoisfromiana($server , $ip)
+{
+	$data='';
+	$f = fsockopen($server , 43 , $errno , $errstr , 3);
+	if (!$f)
+	{
+		return '';
+	}
+	if(!stream_set_timeout($f,3))
+	{
+		die('unable to set set_timeout');
+	}
+	if($f)
+	{
+		fputs($f,"$iprn")
+	}
+	
+	if(!stream_set_timeout($f,3))
+	{
+		die('unable to stream_set_timeout');
+	}
+	stream_set_blocking($f,0);
+	if($f)
+	{
+		while (!feof($f)
+		{
+			$data .= fread ($f , 128);
+		}
+	}
+	return $data;
+}
+
+function get_whois($ip)
+{
+        $ianawhois= getwhoisfromiana('whois.iana.org',$ip);
+        preg_match('@whois.[w.]*@si'.$ianawhois,$data);
+        $whois_server=$data[0];
+        print $whois_server."<br>";
+
+        $whois_data = getwhoisfromiana($whois_server,$ip);
+        return $whois_data;
+}
+
+
+
+
 $ourhost = "www.vcn.bc.ca";
-echo gethostbyname($ourhost)."<br>";
+$ip= gethostbyname($ourhost);
+echo $ip."<br>";
+$mytest = get_whois($ip);
+print_r $mytest;
+
 
 echo "reading the file<br>";
 $myfile = fopen("named.conf","r")or die ("unable to open the file!");
